7df42d20623800c441745340321f0030fc02deb5
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-01-07 15:34:32 +0100

    PS1164ML: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: 0 Manufacturer: digma Model: Plane_1551S_4G_PS1164ML Script version: 2.1.0 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

---
+++ omni_PS1164ML.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from PS1164ML device
+$(call inherit-product, device/digma/PS1164ML/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := PS1164ML
+PRODUCT_NAME := omni_PS1164ML
+PRODUCT_BRAND := DIGMA
+PRODUCT_MODEL := Plane_1551S_4G_PS1164ML
+PRODUCT_MANUFACTURER := digma
